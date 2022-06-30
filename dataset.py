2f9bb6535103c41d96d5add297fefd97c39e6649
epsilon-deltta <kokomong1316@gmail.com>
2022-07-01 02:14:32 +0900

    class variable error in spi10_2nrs class

--- dataset.py
+++ dataset.py
@@ -2,6 +2,24 @@ import torch
 from torch.nn import functional as F
 import pandas as pd
 
+
+def nrs2class(x,pain_type=3):
+    pain = ''
+    
+    if pain_type == 3:
+        if x in [0,1,2,3]:
+            pain = 0
+        elif x in [4,5]:
+            pain = 1
+        else:
+            pain = 2
+    elif pain_type == 2:
+        if x in [0,1,2,3]:
+            pain = 0
+        else:
+            pain = 1
+    return pain
+
 class Spi10_2nrs(torch.utils.data.Dataset):
     def __init__(self,file_path='../data/spi10_2nrs.csv'):
         df = pd.read_csv(file_path)
@@ -11,13 +29,17 @@ class Spi10_2nrs(torch.utils.data.Dataset):
         df['30min'] = df['30min'].map(float)
         df['40min'] = df['40min'].map(float)
         df['50min'] = df['50min'].map(float)
+        df['pacu_nrs'] = df.pacu_nrs.map(lambda x: nrs2class(x,2))
         self.df     = df
 
     def __getitem__(self,idx):
-        x = torch.tensor(df.iloc[idx][:-1].to_numpy())
-        y = torch.tensor([int(df.iloc[idx][-1])])
+        x = torch.tensor(self.df.iloc[idx][:-1].to_numpy())
+        y = torch.tensor([int(self.df.iloc[idx][-1])])
         y = F.one_hot(y,num_classes=2)
         return x,y
     
     def __len__(self):
         return len(self.df)
+    
+    
+  
